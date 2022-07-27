1ab25b5bc5f4df15d336888405d3cd627479303f
Camila Reis <79461028+cxavier6@users.noreply.github.com>
2022-07-27 18:33:42 -0300

    feat: adiciona model da papelaria

---
+++ src/models/PapelariaModel.js
@@ -0,0 +1,11 @@
+class PapelariaModel{
+    constructor(codigo, nome, marca, descricao, preco){
+        this.codigo = codigo
+        this.nome = nome
+        this.marca = marca
+        this.descricao = descricao
+        this.preco = preco
+    }
+}
+
+export default PapelariaModel
