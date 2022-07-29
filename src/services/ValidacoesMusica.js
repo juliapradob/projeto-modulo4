754c79cf9df76ecc2facc340c7ed63b6c8ec7103
Gabriella Rodrigues <gabriella.r.r.2106@gmail.com>
2022-07-28 23:39:42 -0300

    feat: inclui validação completa da entrada da entidade

--- src/services/ValidacoesMusica.js
+++ src/services/ValidacoesMusica.js
@@ -6,5 +6,8 @@ class ValidacoesMusica {
         const preçoValidado=parseInt(preço)
         return preçoValidado==preço
     }
+    static validaProduto (tipo, gênero, artista, preço){
+        return this.validaDados(tipo, gênero, artista) && this.validaPreço (preço)
+    }
 }
 export default ValidacoesMusica
