79265ce2f4c093714da67f859461074d8af08bd8
Kahrinita <karinamottamaciel@hotmail.com>
2022-08-03 11:45:12 -0300

    Commit para adição de código de validação

--- src/services/validacoesInformatica.js
+++ src/services/validacoesInformatica.js
@@ -1,6 +1,71 @@
-class ValidaInformatica {
-    validaTipo ( tipo ) {
-        return tipo.length > 3
-    }
+class ValidacoesInformatica {
+    /**
+     * 
+     * @param {string} tipo 
+     * @returns boolean
+     */
+    static validaTipo (tipo) {
+        return tipo.length >= 2
+    };
 
+        /**
+     * 
+     * @param {string} nome 
+     * @returns boolean
+     */
+    static validaNome(nome) {
+        return nome.length > 3
+    };
+
+    /**
+     * 
+     * @param {string} marca 
+     * @returns boolean
+     */
+    static validaMarca(marca) {
+        return marca.length >= 2
+    };
+
+    /**
+     * 
+     * @param {string} modelo 
+     * @returns boolean
+     */
+    static validaModelo(modelo) {
+        return modelo.length >= 3
+    };
+
+    /**
+     * 
+     * @param {string} valor 
+     * @returns boolean
+     */
+    static validaValor(valor) {
+        return parseInt(valor) == valor;
+    };
+
+    /**
+     * 
+     * @param {string} qtd 
+     * @returns boolean
+     */
+    static validaQtdEmEstoque(qtd) {
+        return parseInt(qtd) == qtd;
+    };
+
+    /**
+     * 
+     * @param {string} tipo 
+     * @param {string} nome 
+     * @param {string} marca 
+     * @param {string} modelo 
+     * @param {string} valor 
+     * @param {string} qtd 
+     * @returns boolean
+     */
+    static validaProduto(tipo, nome, marca, modelo, valor, qtd) {
+        return this.validaTipo(tipo) && this.validaNome(nome) && this.validaMarca(marca) && this.validaModelo(modelo) && this.validaValor(valor) && this.validaQtdEmEstoque(qtd)
+    };
 }
+
+export default ValidacoesInformatica
