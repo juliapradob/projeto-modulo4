6fd3d1e959a767024b245ed13e6631239fba8a8f
Julia Prado <99263001+juliapradob@users.noreply.github.com>
2022-08-03 00:06:18 -0300

    fix: ajusta nome de arquivo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import Clientes from "./src/controllers/Clientes.js"
 import Livros from "./src/controllers/Livros.js"
 import Papelaria from "./src/controllers/Papelaria.js"
 import Informatica from "./src/controllers/Informatica.js"
-import Musica from "./src/controllers/Musica.js"
+import Musicas from "./src/controllers/Musicas.js"
 
 dotenv.config()
 
@@ -22,4 +22,4 @@ Clientes.rotas(app)
 Livros.rotas(app)
 Papelaria.rotas(app)
 Informatica.rotas(app)
-Musica.rotas(app)
+Musicas.rotas(app)
