5d83e6950195064b9e7234a6d0bc589b4d66b95e
Kahrinita <karinamottamaciel@hotmail.com>
2022-08-03 11:41:07 -0300

    Commit para correção de espaçamento

---
+++ src/models/informaticaModels.js
@@ -0,0 +1,13 @@
+class InformaticaModels{
+    constructor (id_produto, tipo_produto, nome_produto, marca_produto, modelo_produto, valor_produto, qtdeEmEstoque_produto) {
+        this.id_produto = id_produto
+        this.tipo_produto = tipo_produto
+        this.nome_produto = nome_produto
+        this.marca_produto = marca_produto
+        this.modelo_produto = modelo_produto
+        this.valor_produto = valor_produto
+        this.qtdeEmEstoque_produto = qtdeEmEstoque_produto
+    }
+}
+
+export default InformaticaModels
