82287bc569a2ae9003d3aa76677a197af0cabdb8
Kahrinita <karinamottamaciel@hotmail.com>
2022-08-03 11:27:47 -0300

    Commit adição de rotas que faltavam - Informática

--- src/controller/informaticaController.js
+++ src/controller/informaticaController.js
@@ -1,26 +1,74 @@
-import InformaticaMetodos from "../DAO/informaticaMetodos.js"
-import InformaticaModels from "../model/informaticaModels.js"
+import InformaticaDAO from "../DAO/InformaticaDAO.js"
+import InformaticaModel from "../models/InformaticaModel.js"
+import ValidacoesInformatica from "../services/ValidacoesInformatica.js"
 
-class InformaticaController {
-    //Código para inserir produto no banco de dados
-    static rotas (app) {
+class Informatica {
+    static rotas(app) {         
         
-        app.post("/inserir", async (req, res)=> {
+        app.get("/informatica", async (req, res) => {
+            try {
+                const resposta = await InformaticaDAO.visualizarDatabaseCompleto();
+                if (resposta.length === 0) {
+                    throw new Error("o database está vazio")
+                }
+                res.status(200).json(resposta)
+            } catch (error) {
+                res.status(404).json(error.message)
+            }
+        });
 
-            const produto = new InformaticaModels (...Object.values(req.body))
-            const resposta = await InformaticaMetodos.inserirProduto(produto)
+        app.get("/informatica/:id", async (req, res) => {
+            try {
+                const produto = await InformaticaDAO.listarProdutoPorId(req.params.id)
+                if (!produto){
+                    throw new Error("Produto não encontrado para esse id")
+                } 
+                res.status(200).json(produto)
+            } catch (error) {
+                res.status(404).json(error.message)
+            }
+        });
 
-            res.status(200).send(resposta)
-        })
+        app.post("/informatica", async (req, res) => {
+            const produtoIsValid = ValidacoesInformatica.validaProduto(...Object.values(req.body))
+            try {
+                if (produtoIsValid) {
+                    const produto = new InformaticaModel(...Object.values(req.body))
+                    const response = await InformaticaDAO.inserirProduto(produto)
+                    res.status(201).json(response)
+                } else {
+                    throw new Error("Requisição com problema, revise o corpo da mesma")
+                }
+            } catch (error) {
+                res.status(400).json(error.message)
+            }
+        });
 
-        app.get("/produtos", async (req, res)=>{
+        app.put("/informatica/:id", async (req, res) => {
+            const produtoIsValid = ValidacoesInformatica.validaProduto(...Object.values(req.body))
+            try {
+                if (produtoIsValid) {
+                    const produto = new InformaticaModel(...Object.values(req.body))
+                    const response = await InformaticaDAO.atualizarProdutoPorId(produto, req.params.id)
+                    res.status(201).json(response)
+                }
+            } catch (error) {
+                res.status(400).json({error: "Produto não foi atualizado"})
+            }
+        });
 
-            const resposta = await InformaticaMetodos.visualizarDatabaseCompleto ();
-            res.status(200).send(resposta)
-        })
-
+        app.delete("/informatica/:id", async (req, res) => {
+            try {
+                const produto = await InformaticaDAO.deletarInformaticaPorId(req.params.id)
+                if(!produto) {
+                    throw new Error("Produto não encontrado para este id")
+                }
+                res.status(200).json(produto)
+            } catch (error) {
+                res.status(404).json(error.message)
+            }
+        });
     }
-
 }
 
-export default InformaticaController
+export default Informatica
